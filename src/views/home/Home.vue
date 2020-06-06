858d8da87305a7c7c4b62fcb020ec23b50f740b0
stars01 <2778115342@qq.com>
2020-06-06 19:42:47 +0800

    home

--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -1,13 +1,47 @@
 <template>
-  <h2>首页</h2>
+<div id="home">
+  <nav-bar class="home-nav">
+    <div slot="center">购物街</div>
+  </nav-bar>
+  <home-swiper :banners='banners'/>
+  <home-recommend-view :recommends='recommends' />
+
+</div>
 </template>
 
 <script>
+import NavBar from '../../components/common/navbar/NavBar'
+import HomeSwiper from './childComps/HomeSwiper'
+import HomeRecommendView from './childComps/HomeRecommendView'
+
+import {getHomeMullData} from "../../network/home"
+
   export default {
-    name: "Home"
+    name: "Home",
+    components: {
+      NavBar,
+      HomeSwiper,
+      HomeRecommendView
+    },
+    data() {
+      return{
+        banners: [],
+        recommends: [],
+      }
+    },
+    created() {
+      //请求数据
+      getHomeMullData().then(res =>{
+        this.banners = res.data.banner.list;
+        this.recommends = res.data.recommend.list;
+      })
+    }
   }
 </script>
 
 <style scoped>
-
+.home-nav {
+  background-color: var(--color-tint);
+  color: #fff;
+}
 </style>
---
+++ src/views/home/childComps/HomeRecommendView.vue
@@ -0,0 +1,49 @@
+<!--  -->
+<template>
+    <div class='recommend'>
+        <div v-for="(item, index) in recommends" :key="index" class="recommend-item">
+            <a :href="item.link">
+                <img :src="item.image" alt="">
+                <p>{{item.title}}</p>
+            </a>
+        </div>
+    </div>
+</template>
+
+<script>
+
+export default {
+    name: 'HomeRecommendView',
+    props: {
+        recommends: {
+            type: Array,
+            default() {
+                return []
+            }
+        }
+    },
+    components: {},
+//方法集合
+    methods: {
+
+    },
+
+}
+</script>
+
+<style>
+.recommend{
+    display: flex;
+    text-align: center;
+    font-size: 14px;
+    padding: 15px 0 10px;
+    border-bottom: 7px solid #aaa;
+}
+.recommend-item{
+    flex: 1;
+}
+.recommend-item img{
+    width: 85%;
+    margin-bottom: 10px;
+}
+</style>
